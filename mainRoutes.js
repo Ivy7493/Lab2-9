4b9f6fd4561439565faa6962346bae86f35b060a
Ivan Von Staden <ivanblizz23@gmail.com>
2022-03-28 15:00:08 +0200

    Lab-2 Deployment commit

---
+++ mainRoutes.js
@@ -0,0 +1,10 @@
+const path = require('path')
+const express = require('express')
+const mainRouter = express.Router()
+mainRouter.get('/about', function (req, res) {
+res.sendFile(path.join(__dirname, 'about.html'))
+})
+mainRouter.get('/', function (req, res) {
+    res.send('Hello World. I\'m a Node app.')
+    })
+module.exports = mainRouter
